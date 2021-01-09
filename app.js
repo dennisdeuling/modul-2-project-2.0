bdb4aac74ee46c6000b183d35c3509dea9e9fb42
Dennis Deuling <dennis.deuling@gmail.com>
2021-01-09 14:10:16 +0100

    changed the document.title

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'dbab - dennis bed & breakfast';
 
 // routes
 app.use('/', require('./routes/apartments'));
